01ae9bd1d721683709bddc861324455ee3753984
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-11-13 14:02:57 -0500

    Completed index file for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
+
+Tag.hasMany(Product, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  }
+})
+
 
 module.exports = {
   Product,
